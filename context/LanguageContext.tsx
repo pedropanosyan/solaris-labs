0fe07502317a7a7010bd0fc4bc28aa817b1d3f9b
pedropanosyan <pedro.panosyan@ing.austral.edu.ar>
2024-07-21 14:15:19 -0300

    Find IP and get language

--- context/LanguageContext.tsx
+++ context/LanguageContext.tsx
@@ -1,6 +1,6 @@
 "use client"
 import {LanguageContextProps, Content} from "@/types/types";
-import {createContext, ReactNode, useContext, useState} from "react";
+import {createContext, ReactNode, useContext, useEffect, useState} from "react";
 import spanishContent from '@/assets/content/spanish.json';
 import englishContent from '@/assets/content/english.json';
 
@@ -9,6 +9,25 @@ const LanguageContext = createContext<LanguageContextProps | undefined>(undefine
 export const LanguageProvider = ({ children }: { children: ReactNode }) => {
     const [language, setLanguage] = useState<'spanish' | 'english'>('english');
     const content: Content = language === 'spanish' ? spanishContent : englishContent;
+
+    const isLanguageSpanish = (countryCode: string) => countryCode === 'ES' || 'AR';
+
+    useEffect(() => {
+        async function fetchLocationAndSetLanguage() {
+            try {
+                const response = await fetch(`https://ipinfo.io/json?token=${process.env.NEXT_PUBLIC_IP_API_TOKEN}`);
+                if (!response.ok) new Error('Failed to fetch user location.');
+                const data = await response.json();
+                const countryCode = data.country;
+                const lang = isLanguageSpanish(countryCode) ? 'spanish' : 'english';
+                setLanguage(lang);
+            } catch (error) {
+                console.error('Error fetching user location:', error);
+            }
+        }
+
+        fetchLocationAndSetLanguage();
+    }, []);
 
     const switchLanguage = (lang: 'spanish' | 'english') => {
         setLanguage(lang);
